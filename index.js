583066462b747186552e962dd791324b5bd3d885
John Freeman <jfreeman08@gmail.com>
2014-08-11 21:40:27 -0400

    simplify programmatic interface

--- index.js
+++ index.js
@@ -29,11 +29,8 @@ var defaultOpts = {
   fs:          fs,
   start:       ['.'],
   maxDepth:    Number.MAX_VALUE,
-  insensitive: false,
-  name:        undefined,
-  path:        undefined,
-  regex:       undefined,
-  prune:       {},
+  filter:      function() { return true },
+  prune:       function() { return false },
 }
 
 /**
@@ -49,87 +46,11 @@ function barrier(n, fn, ctx) {
   }
 }
 
-/**
- * @param glob String Shell glob expression.
- * @param matchSep boolean True if wildcards should match the path component
- * separator (e.g. '/' on Unix).
- * @return regex
- */
-function glob2regex(glob, matchSep) {
-  var any = matchSep ? '.' : '[^/]'
-  return glob
-  .replace(/(\\)?\*/, function($0, $1) {
-    return $1 ? $0 : (any + '*')
-  })
-  .replace(/(\\)?\?/, function($0, $1) {
-    return $1 ? $0 : any
-  })
-}
-
-function pathGlob2regex(glob) {
-  return glob2regex(glob, true)
-}
-
-var alwaysMatch = function() { return true }
-var neverMatch = function() { return false }
-
-function compilePathMatch(opts) {
-  var pats = []
-  if (opts.name) {
-    pats.push('^.*/(?:' + compilePathMatch1(opts.name, glob2regex) + ')$')
-  }
-  if (opts.path) {
-    pats.push('^(?:' + compilePathMatch1(opts.path, pathGlob2regex) + ')$')
-  }
-  if (opts.regex) {
-    pats.push(compilePathMatch1(opts.regex))
-  if (!pats.length) {
-    return
-  }
-  pats = new RegExp(pats.join('|'), opts.insensitive ? 'i' : '')
-  return function(p) { return pats.test(p) }
-}
-
-function compilePathMatch1(pats, template) {
-  if (!util.isArray(pats)) {
-    pats = [pats]
-  }
-  if (template) {
-    pats = pats.map(template)
-  }
-  return pats
-  .map(function(pat) { return '(?:' + pat + ')' })
-  .join('|')
-}
-
-var TEST_METHOD_NAME_FOR_TYPE = {
-  b: 'isBlockDevice',
-  c: 'isCharacterDevice',
-  d: 'isDirectory',
-  f: 'isFile',
-  l: 'isSymbolicLink',
-  p: 'isFIFO',
-  s: 'isSocket',
-}
-
-function compileTypeMatch(opts) {
-  if (!opts.type) {
-    return alwaysMatch
-  }
-  var method = TEST_METHOD_NAME_FOR_TYPE[opts.type]
-  return function(stats) {
-    return stats[method]()
-  }
-}
-
 function FindStream(opts) {
   Readable.call(this, {objectMode: true})
   this.opts      = extend({}, defaultOpts, opts)
   this.paused    = false
   this.buffer    = []
-  this.pathMatch = compilePathMatch(this.opts) || alwaysMatch
-  this.typeMatch = compileTypeMatch(this.opts)
-  this.pruneMatch = compilePathMatch(this.opts.prune) || neverMatch
 
   var start = this.opts.start.map(function(name) {
     return path.resolve(process.cwd(), name)
@@ -157,13 +78,13 @@ FindStream.prototype.pushLevel = function pushLevel(level, prefix, names, done) 
   var bar = barrier(names.length, done)
   names.forEach(function(name) {
     var fullPath = path.join(prefix, name)
-    if (self.pruneMatch(fullPath)) {
-      bar()
-      return
-    }
     self.opts.fs.lstat(fullPath, function(err, stats) {
       if (err) {
         self.emit('error', err)
+      }
+      if (self.opts.prune(fullPath, stats)) {
+        bar()
+        return
       }
       self.tryPushEntry({
         value: {path: fullPath, stats: stats},
@@ -195,7 +116,7 @@ FindStream.prototype.tryPushEntry = function tryPushEntry(entry) {
 FindStream.prototype.pushEntry = function pushEntry(entry) {
   var self = this
   var value = entry.value
-  if (this.pathMatch(value.path) && this.typeMatch(value.stats)) {
+  if (this.opts.filter(value.path, value.stats)) {
     this.paused = !this.push(value) || this.paused
   }
   if (value.stats.isDirectory()) {
