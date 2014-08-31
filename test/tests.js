9e8abbdff10a76c8c936513c0c72d9c6296e58e9
John Freeman <jfreeman08@gmail.com>
2014-08-31 15:54:06 -0400

    name tests

--- test/tests.js
+++ test/tests.js
@@ -4,13 +4,20 @@ var should   = require('should')
 var through2 = require('through2')
 var terminus = require('terminus')
 
+function expectCount(n, done) {
+  return terminus.concat({objectMode: true}, function(files) {
+    files.length.should.equal(n)
+    done()
+  })
+}
+
 describe('find', function() {
   var fs = new Fs({
     entries: {
       a: {
         entries: {
-          x: {},
-          y: {},
+          x1: {},
+          x2: {},
         }
       },
       b: {
@@ -24,10 +31,25 @@ describe('find', function() {
 
   it('should find everything by default', function(done) {
     find({paths: ['/'], fs: fs})
-    .pipe(terminus.concat({objectMode: true}, function(files) {
-      files.length.should.equal(7)
-      done()
-    }))
+    .pipe(expectCount(7, done))
   })
+
+  describe('name expression', function() {
+    it('should match exactly', function(done) {
+      find({paths: ['/'], fs: fs, expr: [{'name': 'x1'}, 'accept']})
+      .pipe(expectCount(1, done))
+    })
+
+    it('should match trailing glob', function(done) {
+      find({paths: ['/'], fs: fs, expr: [{'name': 'x*'}, 'accept']})
+      .pipe(expectCount(2, done))
+    })
+
+    it('should match leading glob', function(done) {
+      find({paths: ['/'], fs: fs, expr: [{'name': '*1'}, 'accept']})
+      .pipe(expectCount(1, done))
+    })
+  })
+
 })
 
